7488cac5634a219baf8b98a8ad9746647f17e233
syedmahmad099 <syedmahmad099@gmail.com>
2017-10-04 20:02:01 +0500

    fixed styling of colors and template

--- app/assets/javascripts/components/modals/layout_type_modal.jsx
+++ app/assets/javascripts/components/modals/layout_type_modal.jsx
@@ -27,13 +27,22 @@ var LayoutTypeModal = React.createClass({
               </div>
               <div className="modal-body">
                 <div className="layout-type-modal">
-                  <div className="template" data-layout-type="single" onClick={this.handleLayoutChange}>
-                    <i className="fa fa-bars fa-3x"></i>
-                    Single
-                  </div>
-                  <div className="template" data-layout-type="double" onClick={this.handleLayoutChange}>
-                    <i className="fa fa-columns fa-3x"></i>
-                    Double
+                  <div className="template-holder">
+                    <div className="template" data-layout-type="single" onClick={this.handleLayoutChange}>
+                      <div classname="active-templete">
+                          Active
+                      </div>
+                      <div className="icon-holder">
+                        <i className="fa fa-bars fa-3x"></i>
+                        <p>Single</p>
+                      </div>
+                    </div>
+                    <div className="template" data-layout-type="double" onClick={this.handleLayoutChange}>
+                      <div className="icon-holder">
+                        <i className="fa fa-columns fa-3x"></i>
+                        <p>Double</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
