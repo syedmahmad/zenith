eaef252e31d7cddbc1d120ce549b3dbeb7c4b004
syedmahmad099 <syedmahmad099@gmail.com>
2017-10-06 22:36:02 +0500

    css apply for show hide

--- app/assets/javascripts/components/options/option_item.jsx
+++ app/assets/javascripts/components/options/option_item.jsx
@@ -1,12 +1,19 @@
 var OptionItem = React.createClass({
 
+  componentDidMount: function(){
+    $('.show_hide_section input').bootstrapToggle();
+  },
+
   render: function() {
   	checked = this.props.model[this.props.item]
   	name = this.props.section+this.props.item+this.props.sectionId
     return (
-    	<div>
-    		<input name={name} data-item-name={this.props.item} className="option_item" onClick={this.props.handleShowHideChange} type="checkbox" checked={checked} data-toggle="toggle"/>
-    	</div>
+    	<div className="box-holder-toggle" >
+        <label>{this.props.item}</label>
+        <span className="toggle-holder">
+    		  <input type="checkbox" data-size="mini" data-toggle="toggle" name={name} data-item-name={this.props.item} className="option_item" onClick={this.props.handleShowHideChange} type="checkbox" checked={checked} data-toggle="toggle"/>
+    	  </span>
+      </div>
     	
     )
   }
