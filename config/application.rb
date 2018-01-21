8daaceedcbe562ab8eab9e6bc1ff2e94695e92fc
syedmahmad <devm.ahmad@gmail.com>
2018-01-22 00:51:53 +0500

    added homepage css and js in assets file

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Zeinth
     config.serve_static_files = true
     config.serve_static_assets = true
     config.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
