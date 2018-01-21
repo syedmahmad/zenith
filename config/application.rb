673a48c26b81226c3424ce777c1ab2b01177d224
syedmahmad <devm.ahmad@gmail.com>
2018-01-22 00:37:14 +0500

    added homepage css and js in assets file

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ module Zeinth
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.serve_static_files = true
     config.serve_static_assets = true
+    config.initialize_on_precompile = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
