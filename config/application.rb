e9af7aa924d2ac20e3e904626e2cca71887a2284
syedmahmad <devm.ahmad@gmail.com>
2018-01-23 23:29:11 +0500

    removed extra stuff

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Zeinth
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_files = true
-    config.serve_static_assets = true
+
     config.initialize_on_precompile = false
     config.assets.initialize_on_precompile = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
